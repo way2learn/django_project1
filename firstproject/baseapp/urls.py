bf252f076ad130483cc28e1f9b9509a0a8cd34c7
Learner <way2learn468@gmail.com>
2023-09-19 07:34:49 -0700

    chat rooms
    updated the chat room messages to visisble in their rooms

--- firstproject/baseapp/urls.py
+++ firstproject/baseapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("logout/", views.logoutUser, name = "logout" ),
     path("login/", views.loginPage, name = "login" ),
+    path("register/", views.registerUser, name = "register" ),
     path("", views.home, name = "home"),
     path("room/<str:pk>", views.room, name = "room"),
     path("create-room/", views.createRoom, name = "create-room"),
--- firstproject/baseapp/views.py
+++ firstproject/baseapp/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 
@@ -19,12 +20,13 @@ rooms = [
 ]
 
 def loginPage(request):
+    page = "login"
     if request.user.is_authenticated:
        return redirect('home')
 
     if request.method == 'POST':
        print("post request:", request.POST)
-       username = request.POST.get('usernamee')
+       username = request.POST.get('usernamee').lower()
        password = request.POST.get('passwordd')
        print("username:", username, "password:", password)
 
@@ -42,12 +44,30 @@ def loginPage(request):
            messages.error(request, "Username or Password are does not exists")
            
            
-    context = {}
+    context = {"page": page}
     return render(request, 'baseapp/login_register.html', context)
     
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerUser(request):
+    page = "register"   # not used 
+    form =  UserCreationForm()
+    if request.method == 'POST':
+       form = UserCreationForm(request.POST)
+       if form.is_valid():
+          user = form.save(commit = False)
+          user.username = user.username.lower()
+          user.save()
+          login(request, user)
+          return redirect("home")
+       else:
+           messages.error(request, "An error occured during the registration")
+
+    return render(request, 'baseapp/login_register.html', {"form": form})
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -59,7 +79,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=int(pk))
-    context = {"rumm": room}
+    room_messages = room.message_set.all().order_by("-created")
+    context = {"rumm": room, "room_messages":room_messages}
     return render(request, 'baseapp/room.html', context)
 
 @login_required(login_url='login')
